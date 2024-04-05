8a258ca7b23613c7a502f1cfaeb436bec57f9553
adsonluis1 <dev.adsonluis@gmail.com>
2024-04-05 00:35:28 -0300

    ex04

---
+++ extra_aula/ex04.py
@@ -0,0 +1,4 @@
+# Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada.
+num = int(input('digite um numero \n'))
+for x in range(11):
+  print(num,' x ',x,' = ',num * x )
