22fec356c843184ee8a7d6b7682defa703e11797
adsonluis1 <dev.adsonluis@gmail.com>
2024-04-05 00:41:20 -0300

    ex07

---
+++ 2_aula/ex07.py
@@ -0,0 +1,5 @@
+# Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+ladoQuadrado = float(input('digite o lado do quadrado \n'))
+areaQuadrado =  pow(ladoQuadrado , 2)
+print('a area do quadrado é',areaQuadrado)
+print('o seu dobro é:', areaQuadrado*2)
