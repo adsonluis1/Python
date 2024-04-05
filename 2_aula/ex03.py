c5f5397705ff7f52968b70cef0f68c7a11246438
adsonluis1 <dev.adsonluis@gmail.com>
2024-04-05 00:39:20 -0300

    ex03

---
+++ 2_aula/ex03.py
@@ -0,0 +1,4 @@
+# Faça um Programa que peça dois números e imprima a soma.
+num = float(input('informe um número \n'))
+num2 = float(input('informe outro número \n'))
+print('a soma dos números foi:', num + num2)
