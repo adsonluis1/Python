efc40c3926cc2df402e8e6f3747237515253ea7d
adsonluis1 <dev.adsonluis@gmail.com>
2024-04-05 00:39:51 -0300

    ex04

---
+++ 2_aula/ex04.py
@@ -0,0 +1,6 @@
+# Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+nota = float(input('informe sua nota \n'))
+nota2 = float(input('informe sua nota \n'))
+nota3 = float(input('informe sua nota \n'))
+nota4 = float(input('informe sua nota \n'))
+print('sua media foi de:', (nota + nota2 + nota3 + nota4) / 4)
