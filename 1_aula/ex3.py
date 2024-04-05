c96c6460ae185ff2d57bb11e8b050859081601aa
adsonluis1 <dev.adsonluis@gmail.com>
2024-04-04 23:51:48 -0300

    third code in py~

---
+++ 1_aula/ex3.py
@@ -0,0 +1,6 @@
+# terceiro codigo em py
+nota1 = int(input('digite a primeira nota \n'))
+nota2 = int(input('digite a segunda nota \n'))
+nota3 = int(input('digite a terceira nota \n'))
+media = (nota1 + nota2 + nota3) / 3
+print(round(media,2))
