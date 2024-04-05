029a9133dc2d9134c77081ce8dc425ba5ed98af5
adsonluis1 <dev.adsonluis@gmail.com>
2024-04-05 00:34:51 -0300

    ex03

---
+++ extra_aula/ex03.py
@@ -0,0 +1,5 @@
+# Escreva um programa que leia um valor em metros e exiba convertido em centímetros e milímetros.
+medidaM = float(input('digite a medida em metros \n'))
+medidaCm = medidaM * 100
+medidaMm = medidaM * 1000
+print('medida em metros', medidaM, 'medida em centimetros', medidaCm,'Cm', 'medida em milimetros', medidaMm,'Mm')
