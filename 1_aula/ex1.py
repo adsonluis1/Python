0970c7c9113fd19004aadf0205d07501598f0c3f
adsonluis1 <dev.adsonluis@gmail.com>
2024-04-04 23:45:44 -0300

    Initial

---
+++ 1_aula/ex1.py
@@ -0,0 +1,5 @@
+# Primeiro codigo em py
+n1 = input('digite um numero \n')
+n2 = input('digite um numero \n')
+total = int(n1) + int(n2)
+print(total)
---
+++ 1_aula/ex2.py
@@ -0,0 +1,8 @@
+# Segundo codigo em py
+anoAtual = 2024
+print('Qual seu nome ?')
+nome = input()
+print('que ano vc nasceu ?')
+anoNascimento = input()
+idade = int(anoAtual) - int(anoNascimento)
+print('Ola ', nome, 'voce tem ', idade, 'anos')
