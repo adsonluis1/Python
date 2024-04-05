3fb5b46b932fb5125aaf2ff75b026fa3e493564c
adsonluis1 <dev.adsonluis@gmail.com>
2024-04-05 00:42:57 -0300

    ex10

---
+++ 2_aula/ex10.py
@@ -0,0 +1,4 @@
+# Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
+temperaturaCelcios = float(input('digite a temperatura em Celsius \n'))
+temperaturaFahrenheit = temperaturaCelcios * 1.8 + 32
+print(temperaturaFahrenheit, '°C')
