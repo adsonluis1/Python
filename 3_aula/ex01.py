2ecd8204fbb87a974888eac2ca8e161a1206ef39
adsonluis1 <dev.adsonluis@gmail.com>
2024-04-09 23:17:33 -0300

    ex01

---
+++ 3_aula/ex01.py
@@ -0,0 +1,7 @@
+# Faça um Programa que peça dois números e imprima o maior deles.
+num1 = float(input('digite um número \n'))
+num2 = float(input('digite um número \n'))
+if(num1 > num2):
+  print(num1)
+else:
+  print(num2)
