f2dfa9ad8ca5acb3fb5cb37730d7a0586224589e
adsonluis1 <dev.adsonluis@gmail.com>
2024-04-09 23:27:32 -0300

    ex06

---
+++ 3_aula/ex06.py
@@ -0,0 +1,6 @@
+# Faça um Programa que leia três números e mostre o maior deles.
+num1 = float(input('digite um numerr \n'))
+num2 = float(input('digite um numerr \n'))
+num3 = float(input('digite um numerr \n'))
+
+print('o maior numero é:',max(num1, num2, num3))
