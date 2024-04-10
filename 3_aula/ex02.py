c631c60a8ecd36c8c95f89dce298c7d088d6b880
adsonluis1 <dev.adsonluis@gmail.com>
2024-04-09 23:18:29 -0300

    ex02

---
+++ 3_aula/ex02.py
@@ -0,0 +1,6 @@
+# Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+num = float(input('digite um número \n'))
+if num>0:
+  print('positivo')
+else:
+  print('negativo')
