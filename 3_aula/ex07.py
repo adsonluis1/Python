8a32bbd5ec4ed7063b4e0e32bef2e479adb427af
adsonluis1 <dev.adsonluis@gmail.com>
2024-04-09 23:28:20 -0300

    ex07

---
+++ 3_aula/ex07.py
@@ -0,0 +1,7 @@
+# Faça um Programa que leia três números e mostre o maior e o menor deles.
+num1 = float(input('digite um numero \n'))
+num2 = float(input('digite um numero \n'))
+num3 = float(input('digite um numero \n'))
+
+print('o maior numero é:',max(num1, num2, num3))
+print('o menor numero é:',min(num1,num2,num3))
